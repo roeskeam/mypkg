92aada4572f037f584816c71b0654089f2494981
Annalea <annalearoeske@gmail.com>
2018-02-20 11:12:00 -0800

    first commit

---
+++ mod1/vars.py
@@ -0,0 +1,1 @@
+x="mypkg/mod1"
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='mypkg',
+	  version='0.1',
+	  description='test package',
+	  url='http://github.com/roeskeam/mypkg',
+	  author='Annalea',
+	  author_email='annalearoeske@gmail.com',
+#	  license='MIT',
+	  packages=['mod1','mod2','mod3'], zip_safe=False)
